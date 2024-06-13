aa21d869dd588c1dc4c39d00815a071ac2199792
Kimi Dandy <kimidandy2004@gmail.com>
2024-06-14 02:13:47 +0700

    Setup backend and frontend 1

--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
-const express =  require('express')
-const cors =  require('cors')
-const dotenv =  require('dotenv')
-const {testConnection} = require('./Database/db.js')
-const routerKonser = require('./Router/tambahKonser.js')
-const routerUser = require('./Router/tambahUser.js')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { testConnection } = require('./Database/db.js');
+const routerKonser = require('./Router/tambahKonser.js');
+const routerUser = require('./Router/tambahUser.js');
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(routerKonser);
 app.use(routerUser);
 
-app.listen(process.env.APP_PORT, async () => {
+// Route untuk root path
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, async () => {
     await testConnection();
-    console.log(`http://localhost:${process.env.APP_PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
